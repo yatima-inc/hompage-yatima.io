c2b96ed6503557a7846398cd0698639b3ebda406
Anders Christiansen Sørby <anders@sorby.xyz>
2021-07-01 21:29:54 +0200

    Add flake.nix

--- content/pages/about/index.md
+++ content/pages/about/index.md
@@ -1,9 +1,9 @@
 ---
 title: 'About'
-date: 2018-12-06T09:29:16+10:00
+date: 2020-07-01T09:29:16+10:00
 layout: 'about'
 heroHeading: 'About Us'
-heroSubHeading: "Established in 1985, we're a team of advisors that puts your business first."
+heroSubHeading: "Developing the Yatima language."
 heroBackground: 'https://source.unsplash.com/sO-JmQj95ec/1600x1000'
 ---
 
---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "The homepage of Yatima Inc";
+ 
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    utils.url = "github:numtide/flake-utils";
+    hugo-theme = {
+      url = "github:zerostaticthemes/hugo-hero-theme";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, utils, hugo-theme }: 
+    utils.lib.eachDefaultSystem (system:
+    let
+      name = "homepage-yatima-io";
+      pkgs = import nixpkgs {};
+      buildInputs = with pkgs; [
+        hugo
+        hugo-theme
+      ];
+      linkThemeScript = ''
+        mkdir -p themes
+        ln -snf "${hugo-theme}" themes/hugo-hero-theme
+      '';
+      project = pkgs.stdenv.mkDerivation {
+        inherit name buildInputs system;
+        src = ./.;
+        configurePhase = linkThemeScript;
+        buildPhase = ''
+          hugo
+        '';
+        installPhase = ''
+          cp -r public $out
+        '';
+      };
+    in
+    {
+      packages.${name} = project;
+      defaultPackage = self.packages.${system}.${name};
+      devShell = pkgs.mkShell {
+        inherit buildInputs;
+        shellHook = linkThemeScript;
+      };
+
+    });
+}
